3f13017526b4184fe27b4c47858f65803192c8bd
jq1004 <junqingl@usc.edu>
2019-06-13 13:30:22 -0400

    connect frontend with RESTful Service

--- src/main/java/com/colin/rest/webservices/restfulwebservices/TodoResource.java
+++ src/main/java/com/colin/rest/webservices/restfulwebservices/TodoResource.java
@@ -3,6 +3,7 @@ package com.colin.rest.webservices.restfulwebservices;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.colin.rest.webservices.restfulwebservices.todo.Todo;
 import com.colin.rest.webservices.restfulwebservices.todo.TodoHardcodedService;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class TodoResource {
 
