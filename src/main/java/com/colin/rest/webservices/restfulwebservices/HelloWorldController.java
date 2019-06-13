fa7189c44fcc1bd9a748c55f3f1379908db8db43
jq1004 <junqingl@usc.edu>
2019-06-13 11:38:01 -0400

    call http service with path variable

--- src/main/java/com/colin/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/colin/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.colin.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class HelloWorldController {
 	
@@ -17,7 +19,7 @@ public class HelloWorldController {
 	
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World");
+		return new HelloWorldBean("Hello World - changed");
 	}
 	
 	@GetMapping(path="/hello-world/path-variable/{name}")
