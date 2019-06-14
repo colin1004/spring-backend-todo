4a427c6e27b46306887f70f68045dbc1a4cfeda5
jq1004 <junqingl@usc.edu>
2019-06-14 08:31:54 -0400

    REST API for update todo-put request

--- src/main/java/com/colin/rest/webservices/restfulwebservices/TodoResource.java
+++ src/main/java/com/colin/rest/webservices/restfulwebservices/TodoResource.java
@@ -3,11 +3,14 @@ package com.colin.rest.webservices.restfulwebservices;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.colin.rest.webservices.restfulwebservices.todo.Todo;
@@ -37,5 +40,15 @@ public class TodoResource {
 			return ResponseEntity.noContent().build();
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(
+			@PathVariable String name,
+			@PathVariable long id, @RequestBody Todo todo
+			){
+			
+			Todo todoUpdated = todoService.save(todo);
+			return new ResponseEntity<Todo>(todo, HttpStatus.OK);
 	}
 }
